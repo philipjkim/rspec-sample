e6c6916f9e0c6974d32818ce91db64eb2519095f
PJ Kim <pj@kakao.com>
2012-09-11 20:24:06 +0900

    added Bowling to pass the test; modified typo in spec

--- bowling_spec.rb
+++ bowling_spec.rb
@@ -1,8 +1,8 @@
-require 'bowling'
+require_relative 'bowling'
 
 describe Bowling, "#score" do
   it "returns 0 for all gutter game" do
-    bowling - Bowling.new
+    bowling = Bowling.new
     20.times { bowling.hit(0) }
     bowling.score.should eq(0)
   end
